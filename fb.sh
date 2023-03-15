b4dd1275ded83488ce330fda4998b2f05acb131b
Xingyue Huang <49787534+HxyScotthuang@users.noreply.github.com>
2023-03-15 20:41:26 +0000

    Create fb.sh

---
+++ fb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --time=11:59:59
+#SBATCH --job-name=rgcn
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=short
+#SBATCH --gres=gpu:v100:1 
+
+module purge
+module load Anaconda3
+source activate $DATA/myenv
+
+
+python main.py --negative-sample 32 --n-epochs 20 --dataset FB15k-237 --gpu 0 --n-bases 30 --graph-batch-size 32
