8a915fb2bd37db09bd2e0a9b69f8a95a997992ac
Xingyue Huang <49787534+HxyScotthuang@users.noreply.github.com>
2023-03-15 20:42:11 +0000

    Create wn.sh

---
+++ wn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --time=11:59:59
+#SBATCH --job-name=rgcn
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=short
+#SBATCH --gres=gpu:v100:1 
+
+module purge
+module load Anaconda3
+source activate $DATA/myenv
+
+
+python main.py --negative-sample 32 --n-epochs 20 --dataset wn18rr --gpu 0 --n-bases 5 --graph-batch-size 32
